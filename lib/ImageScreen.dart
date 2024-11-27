cbef52e1a23ed32fd697f0542584a6b930b87494
Devansh <devanshdhopte@gmail.com>
2024-11-28 00:28:06 +0530

    Carousel Updation done

--- lib/ImageScreen.dart
+++ lib/ImageScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -11,7 +10,6 @@ import 'package:palette_generator/palette_generator.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
-
 
 class ImageScreen extends StatefulWidget {
   final List<String> imageUrls;
@@ -25,17 +23,19 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   Color? dominantColor;
-  Color? undominantColor;
   int currentIndex = 0;
   List relatedImages = [];
   bool isLoadingImages = false;
+  Map<String, List<dynamic>> cachedRelatedImages = {}; // Cache for related images
 
   @override
   void initState() {
     super.initState();
     currentIndex = widget.initialIndex;
     _getDominantColor(widget.imageUrls[currentIndex]);
-    _fetchRelatedImages(widget.imageUrls[currentIndex]);
+    if (relatedImages.isEmpty) {
+      _fetchRelatedImages(widget.imageUrls[currentIndex]);
+    }
   }
 
   Future<void> _getDominantColor(String imageUrl) async {
@@ -63,13 +63,22 @@ class _ImageScreenState extends State<ImageScreen> {
     }).toList();
   }
 
-  Future<void> _fetchRelatedImages(String keyword) async {
+  Future<void> _fetchRelatedImages(String query) async {
     setState(() {
       isLoadingImages = true;
     });
 
-    String unsplashUrl = 'https://api.unsplash.com/search/photos?query=$keyword&per_page=15';
-    String pexelsUrl = 'https://api.pexels.com/v1/search?query=$keyword&per_page=15';
+    // Check cache first
+    if (cachedRelatedImages.containsKey(query)) {
+      setState(() {
+        relatedImages = cachedRelatedImages[query]!;
+        isLoadingImages = false;
+      });
+      return;
+    }
+
+    String unsplashUrl = 'https://api.unsplash.com/search/photos?query=$query&per_page=15';
+    String pexelsUrl = 'https://api.pexels.com/v1/search?query=$query&per_page=15';
 
     try {
       final unsplashResponse = await http.get(
@@ -85,9 +94,6 @@ class _ImageScreenState extends State<ImageScreen> {
           'Authorization': '4qZ2txUCt5XrsjKk7YrkDanR5BvZwuyt5xR4zPW5kWL6LfkjxZapfsQM',
         },
       );
-
-      print("Pexels Response Status: ${pexelsResponse.statusCode}");
-      print("Unsplash Response Status: ${unsplashResponse.statusCode}");
 
       if (pexelsResponse.statusCode == 200 && unsplashResponse.statusCode == 200) {
         Map<String, dynamic> pexelsResult = jsonDecode(pexelsResponse.body);
@@ -114,11 +120,14 @@ class _ImageScreenState extends State<ImageScreen> {
         List combinedImages = _filterUniqueImages([...filteredPexelsPhotos, ...filteredUnsplashPhotos]);
         combinedImages.shuffle(Random());
 
+        // Cache related images
+        cachedRelatedImages[query] = combinedImages;
+
         if (mounted) {
           setState(() {
             relatedImages = combinedImages;
+            isLoadingImages = false;
           });
-          print("Fetched Related Images: ${relatedImages.length}");
         }
       } else {
         throw Exception("Failed to load related images");
@@ -133,7 +142,6 @@ class _ImageScreenState extends State<ImageScreen> {
       }
     }
   }
-
 
   Future<void> setWallpaper() async {
     try {
@@ -170,6 +178,27 @@ class _ImageScreenState extends State<ImageScreen> {
   }
 
   Future<void> setLockScreen() async {
+    String? imageUrl;
+
+    // Determine if the current image is from main images or related images
+    if (currentIndex < widget.imageUrls.length) {
+      imageUrl = widget.imageUrls[currentIndex];
+    } else {
+      int relatedIndex = currentIndex - widget.imageUrls.length;
+      if (relatedIndex < relatedImages.length) {
+        imageUrl = relatedImages[relatedIndex]['src']['large2x'];
+      }
+    }
+
+    if (imageUrl == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No image available'),
+        ),
+      );
+      return;
+    }
+
     try {
       int location = WallpaperManager.LOCK_SCREEN;
       var file = await DefaultCacheManager().getSingleFile(widget.imageUrls[currentIndex]);
@@ -204,12 +233,33 @@ class _ImageScreenState extends State<ImageScreen> {
   }
 
   Future<void> setBoth() async {
+    String? imageUrl;
+
+    // Determine if the current image is from main images or related images
+    if (currentIndex < widget.imageUrls.length) {
+      imageUrl = widget.imageUrls[currentIndex];
+    } else {
+      int relatedIndex = currentIndex - widget.imageUrls.length;
+      if (relatedIndex < relatedImages.length) {
+        imageUrl = relatedImages[relatedIndex]['src']['large2x'];
+      }
+    }
+
+    if (imageUrl == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No image available'),
+        ),
+      );
+      return;
+    }
+
     try {
       int location = WallpaperManager.BOTH_SCREEN;
       var file = await DefaultCacheManager().getSingleFile(widget.imageUrls[currentIndex]);
       bool result = await WallpaperManager.setWallpaperFromFile(
           file.path, location);
-      
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           backgroundColor: dominantColor,
@@ -236,22 +286,37 @@ class _ImageScreenState extends State<ImageScreen> {
   }
 
   Future<void> saveToGallery() async {
-    if(widget.imageUrls.isEmpty || currentIndex < 0 || currentIndex >= widget.imageUrls.length) {
+    String? imageUrl;
+
+    // Determine if the current image is from main images or related images
+    if (currentIndex < widget.imageUrls.length) {
+      imageUrl = widget.imageUrls[currentIndex];
+    } else {
+      int relatedIndex = currentIndex - widget.imageUrls.length;
+      if (relatedIndex < relatedImages.length) {
+        imageUrl = relatedImages[relatedIndex]['src']['large2x'];
+      }
+    }
+
+    // If no image URL is found, show a message and exit
+    if (imageUrl == null) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('No image available to save',),
+        const SnackBar(
+          content: Text('No image available to save'),
         ),
       );
       return;
     }
 
     try {
-      var file = await DefaultCacheManager().getSingleFile(widget.imageUrls[currentIndex]);
+      var file = await DefaultCacheManager().getSingleFile(imageUrl);
       await ImageGallerySaver.saveFile(file.path);
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           backgroundColor: dominantColor,
-          content: Text('Saved to Gallery',
+          content: Text(
+            'Saved to Gallery',
             style: GoogleFonts.farro(
               fontSize: 20,
               color: Colors.white,
@@ -263,7 +328,8 @@ class _ImageScreenState extends State<ImageScreen> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           backgroundColor: dominantColor,
-          content: Text('Failed to save: $e',
+          content: Text(
+            'Failed to save: $e',
             style: GoogleFonts.aclonica(
               fontSize: 10,
               color: Colors.white,
@@ -275,23 +341,37 @@ class _ImageScreenState extends State<ImageScreen> {
   }
 
   Future<void> shareImage() async {
-    if(widget.imageUrls.isEmpty || currentIndex < 0 || currentIndex >= widget.imageUrls.length) {
+    String? imageUrl;
+
+    // Determine if the current image is from main images or related images
+    if (currentIndex < widget.imageUrls.length) {
+      imageUrl = widget.imageUrls[currentIndex];
+    } else {
+      int relatedIndex = currentIndex - widget.imageUrls.length;
+      if (relatedIndex < relatedImages.length) {
+        imageUrl = relatedImages[relatedIndex]['src']['large2x'];
+      }
+    }
+
+    // If no image URL is found, show a message and exit
+    if (imageUrl == null) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('No image available to share',),
+        const SnackBar(
+          content: Text('No image available to share'),
         ),
       );
       return;
     }
 
     try {
-      var file = await DefaultCacheManager().getSingleFile(widget.imageUrls[currentIndex]);
+      var file = await DefaultCacheManager().getSingleFile(imageUrl);
       final XFile xFile = XFile(file.path);
       Share.shareXFiles([xFile], text: 'Check out this amazing wallpaper from Wallviz !!');
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Failed to share : $e',
+          content: Text(
+            'Failed to share: $e',
             style: GoogleFonts.aclonica(
               fontSize: 10,
               color: Colors.white,
@@ -301,6 +381,7 @@ class _ImageScreenState extends State<ImageScreen> {
       );
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -397,37 +478,53 @@ class _ImageScreenState extends State<ImageScreen> {
         children: [
           Expanded(
             child: CarouselSlider.builder(
-              itemCount: widget.imageUrls.length,
+              itemCount: widget.imageUrls.length + relatedImages.length,
               itemBuilder: (context, index, realIndex) {
-                if (isLoadingImages) {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      color: toggleColor,
-                    ),
-                  );
-                } else if (index == 0) {
-                  return Image.network(
-                    widget.imageUrls[currentIndex],
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Center(
-                        child: Text('Image not available'),
-                      );
-                    },
-                  );
+                String imageUrl;
+                if (index < widget.imageUrls.length) {
+                  // Main image
+                  imageUrl = widget.imageUrls[index];
                 } else {
-                  return Image.network(
-                    relatedImages[index - 1]['src']['large2x'],
+                  // Related image
+                  int relatedIndex = index - widget.imageUrls.length;
+                  if (relatedImages.isNotEmpty && relatedIndex < relatedImages.length) {
+                    imageUrl = relatedImages[relatedIndex]['src']['large2x'];
+                  } else {
+                    // Fallback in case relatedImages is shorter than expected
+                    imageUrl = widget.imageUrls[0]; // Or any default image
+                  }
+                }
+
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      currentIndex = index;
+                      _getDominantColor(imageUrl);
+                    });
+                  },
+                  child: Image.network(
+                    imageUrl,
                     fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if(loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                              : null,
+                        ),
+                      );
+                    },
                     errorBuilder: (context, error, stackTrace) {
                       return Center(
                         child: Text('Image not available'),
                       );
                     },
-                  );
-                }
+                  )
+                );
               },
               options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.7,
                 initialPage: 0,
                 enableInfiniteScroll: true,
                 enlargeCenterPage: true,
@@ -435,19 +532,21 @@ class _ImageScreenState extends State<ImageScreen> {
                 onPageChanged: (index, reason) {
                   setState(() {
                     currentIndex = index;
-                    if (index == 0) {
-                      _getDominantColor(widget.imageUrls[currentIndex]);
-                      if(relatedImages.isEmpty) {
-                        _fetchRelatedImages(widget.imageUrls[currentIndex]);
-                      }
-                    }
-                    else if (index > 0 && index <= relatedImages.length) {
-                      int relatedIndex = index - 1;
+
+                    if (index < widget.imageUrls.length) {
+                      // Main images
+                      _getDominantColor(widget.imageUrls[index]);
+                    } else if (index < widget.imageUrls.length + relatedImages.length) {
+                      // Related images
+                      int relatedIndex = index - widget.imageUrls.length;
                       _getDominantColor(relatedImages[relatedIndex]['src']['large2x']);
-                      _fetchRelatedImages(relatedImages[relatedIndex]['src']['large2x']);
+                    } else {
+                      // Out of range - prevent further errors
+                      print('Index out of range: $index');
                     }
                   });
                 },
+
               ),
             ),
           ),
@@ -469,14 +568,19 @@ class _ImageScreenState extends State<ImageScreen> {
 
   Widget _actionIcon(IconData icon, Color color, VoidCallback onPressed) {
     final screenSize = MediaQuery.of(context).size;
-    final width = screenSize.width;
     final height = screenSize.height;
 
-    return Container(
-      height: height * 0.18,
-      width: width * 0.18,
-      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
-      child: IconButton(icon: Icon(icon, color: dominantColor, size: width * 0.085,), onPressed: onPressed),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.all(height * 0.015),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color.withOpacity(0.1),
+        ),
+        child: Icon(icon, color: color, size: height * 0.035),
+      ),
     );
   }
+
 }
